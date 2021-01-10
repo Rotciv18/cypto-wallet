35f760affa2206c26641f76f146e8531f8eee339
Victor Batista <v.rotciv@hotmail.com>
2021-01-10 02:09:12 -0300

    seeds; rails generate task dev setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cryptocoin.create(
+  description: "Bitcoin",
+  acronym: "BTC",
+  image_url: "https://www.comocomprarcriptomoedas.com/wp-content/uploads/2018/02/bitcoin-logo.png"
+)
+
+Cryptocoin.create(
+  description: "Ethereum",
+  acronym: "ETC",
+  image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj4sPvxHepSWg-lkodct5SMVvhJoKVPN3b0Q&usqp=CAU"
+)
---
+++ lib/tasks/dev.rake
@@ -0,0 +1,7 @@
+namespace :dev do
+  desc "Configura o ambiente de desenvolvimento"
+  task setup: :environment do
+    %x(rails db:drop db:create db:migrate db:seed)
+  end
+
+end
