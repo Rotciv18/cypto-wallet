e97d1aed9fbd89496a3d562653b6c9dfa4519b5f
Victor Batista <v.rotciv@hotmail.com>
2021-01-11 17:54:57 -0300

    adding jquery and notify.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
+const path = require('path')
+
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    notify: path.resolve(path.join(__dirname, 'vendor/assets/javascripts'))
+  })
+);
 
 module.exports = environment
